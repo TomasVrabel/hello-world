95220222ff2b32c64e9f34197c1d80c78ba3f6e8
Tomas Vrabel <tomas.vrabel@solarwinds.com>
2021-01-28 21:42:32 +0100

    Kotlin 1st build step

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
 version = "2020.2"
 
@@ -7,4 +8,14 @@ project {
 		// Disable editing of project and build settings from the UI to avoid issues with TeamCity
         param("teamcity.ui.settings.readOnly", "true")
     }
+	
+	buildType {
+		id("HelloWorld")
+		name = "Hello world"
+		steps {
+			script {
+				scriptContent = "echo 'Hello world!'"
+			}
+		}
+	}
 }
