d9fc8851dde6b90fb4fdc74a0cb865db1e9f7e6a
Tomas Vrabel <tomas.vrabel@solarwinds.com>
2021-01-28 22:11:10 +0100

    disable TC UI

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -4,6 +4,12 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 version = "2020.2"
 
 project {
+
+  params {
+	// Disable editing of project and build settings from the UI to avoid issues with TeamCity
+    param("teamcity.ui.settings.readOnly", "true")
+  }
+	
   buildType {
     id("HelloWorld")
     name = "Hello world"
