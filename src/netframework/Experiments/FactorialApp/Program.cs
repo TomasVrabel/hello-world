34403852868a20b563fceb4bf516724e47923e3a
Tomas Vrabel <tomas.vrabel@solarwinds.com>
2021-01-30 21:58:00 +0100

    netframework demo

---
+++ src/netframework/Experiments/FactorialApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FactorialApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 10;
+
+            long fact = 1;
+            for (var i = 1; i <= n; i++)
+            {
+                fact *= i;
+            }
+
+            Console.WriteLine("Factorial({0}) = {1}", n, fact);
+        }
+    }
+}
