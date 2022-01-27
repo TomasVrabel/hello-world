f30c94a333d709fcb208354e7ac267db506d1441
Tomas Vrabel <Tomas.Vrabel@solarwinds.com>
2022-01-27 21:31:30 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -38,10 +38,12 @@ object Build : BuildType({
             name = "start"
             scriptContent = "echo 'Hello world!'"
         }
+        /*
         dotnetBuild {
             name = "build"
             projects = "src/dotnet/HelloWorld"
         }
+        */
         script {
             name = "finish"
             scriptContent = "echo 'Build finished!'"
